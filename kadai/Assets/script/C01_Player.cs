759038a58672fb6626848e27438550d688be16fd
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 13:34:06 +0900

    FPSControllerスクリプトを削除。Playerスクリプトを追加

---
+++ kadai/Assets/script/C01_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C01_Player : MonoBehaviour {
+    private CharacterController charaCon;
+    private Vector3 move = Vector3.zero;
+    private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        charaCon = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        charaCon.Move(move * speed * Time.deltaTime);
+	}
+}
