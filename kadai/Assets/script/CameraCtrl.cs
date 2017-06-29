633e5d438db921047654a07f7c584112c3104b06
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 14:33:03 +0900

    カメラ切り替え機能の追加。スクリプトCameraCtrlを追加

---
+++ kadai/Assets/script/CameraCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour {
+
+    private PlayerMove PM;
+
+	// Use this for initialization
+	void Start () {
+        PM = GameObject.FindWithTag("Player").GetComponent<PlayerMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            changeCameraMode();
+        }
+
+	}
+
+    //カメラを切り替える処理
+    private void changeCameraMode()
+    {
+        if(transform.parent == null)
+        {
+            transform.parent =        PM.transform;
+            transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            transform.parent = null;
+            transform.position = new Vector3(0, 4, -10);
+        }
+    }
+}
