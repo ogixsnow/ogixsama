9c62f267ad59e26aa7cfcbab4cdd9a50445de8b1
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 13:56:02 +0900

    ジャンプ処理の修正（ずっとジャンプしないように修正）

--- kadai/Assets/script/PlayerMove.cs
+++ kadai/Assets/script/PlayerMove.cs
@@ -8,7 +8,7 @@ public class PlayerMove : MonoBehaviour {
     private float speed = 5.0f;
 
     private const float GRAVITY = 9.8f;
-    public float jumpPower = 2.0f;
+    private float jumpPower = 2.5f;
 
     // Use this for initialization
     void Start () {
@@ -21,9 +21,12 @@ public class PlayerMove : MonoBehaviour {
         move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         move *= speed;
         move.y += y;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (charaCon.isGrounded)
         {
-            move.y = jumpPower;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                move.y = jumpPower;
+            }
         }
         move.y -= GRAVITY * Time.deltaTime;
         charaCon.Move(move * speed * Time.deltaTime);
