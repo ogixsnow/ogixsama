7d051bc9d0342dee1fa5595b58114d8b31de64a6
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 13:17:29 +0900

    スクリプト FPSControllerを追加

---
+++ kadai/Assets/script/FPSController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour {
+
+    public float speed = 2f;
+    CharacterController player;
+
+    float moveFB;
+    float moveLR;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveFB = Input.GetAxis("Vertical") * speed;
+        moveLR = Input.GetAxis("Horizontal") * speed;
+
+        Vector3 movement = new Vector3(moveLR,moveFB,0);
+        movement = transform.rotation * movement;
+	}
+}
