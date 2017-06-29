5017e0b169c36f60559678a4e0c370e6df213eff
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 11:41:50 +0900

    矢印キーで左右前奥移動

---
+++ kadai/Assets/script/inputkeys.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputkeys : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(1, 1, 1);
+        Vector3 v = this.transform.position;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v.x += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            v.z += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            v.z -= 0.05f;
+        }
+        this.transform.position = v;
+    }
+}
