27e8c899468f340e437e236eac5a3dc2721ccff2
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 13:42:38 +0900

    PlayerMoveを修正。重力を追加

--- kadai/Assets/script/PlayerMove.cs
+++ kadai/Assets/script/PlayerMove.cs
@@ -7,14 +7,20 @@ public class PlayerMove : MonoBehaviour {
     private Vector3 move = Vector3.zero;
     private float speed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
+    private const float GRAVITY = 9.8f;
+
+    // Use this for initialization
+    void Start () {
         charaCon = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float y = move.y;
         move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        move *= speed;
+        move.y += y;
+        move.y -= GRAVITY * Time.deltaTime;
         charaCon.Move(move * speed * Time.deltaTime);
 	}
 }
