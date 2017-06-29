ca6bc706ec749f103ee347ce286e157e0856f431
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-29 17:30:28 +0900

    Weaponスクリプトを作る。武器切り替えシステムの下準備。

---
+++ kadai/Assets/script/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    private int type =0;
+    private int num = 2;
+
+    public void changeWeapon()
+    {
+        type = (type + 1) % num;
+        Debug.Log("現在の武器:" + type);
+    }
+}
