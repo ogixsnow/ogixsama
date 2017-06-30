f2305e5752856363871f2b4c1e1d6c4df42f5a32
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-30 10:27:11 +0900

    Bomスクリプトを作成。3秒後に消滅

---
+++ kadai/Assets/script/Bom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("bom");//コルーチン開始
+	}
+	
+	IEnumerator bom()
+    {
+        yield return new WaitForSeconds(2.5f);//2.5秒後に処理
+        Destroy(gameObject);
+    }
+}
