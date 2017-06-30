6b47d7cf0031256d835de2fe1ce1212539d0a375
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-30 15:32:30 +0900

    Uiスクリプトを修正。ボムのテキストを追加

--- kadai/Assets/script/Ui.cs
+++ kadai/Assets/script/Ui.cs
@@ -5,8 +5,44 @@ using UnityEngine.UI;
 
 public class Ui : MonoBehaviour {
     public Text[] text_gun_num;
+    public Text text_bom;
 
-	//残段数を変更
+    //手榴弾のテキスト変更
+    public void changeText_Bom(bool used)
+    {
+        if(text_bom != null)
+        {
+            text_bom.text = used ? "装填中" : "使用可";
+        }
+    }
+
+    //武器タイプによるテキストの表示オン/オフ
+    public void changeText_enable(int type)
+    {
+        switch (type)
+        {
+            case 0:
+                foreach(Text t in text_gun_num) { t.enabled = true; }
+                text_bom.enabled = false;
+                break;
+            case 1:
+                foreach(Text t in text_gun_num) { t.enabled = false; }
+                text_bom.enabled = true;
+                break;
+        }
+    }
+
+    //テキスト初期化用
+    public void initialize(int type,int num , bool used)
+    {
+        changeText_enable(type);
+
+        changeText_GunNum(num);
+        changeText_Bom(used);
+    }
+
+
+    //残段数を変更
     public void changeText_GunNum(int num)
     {
         foreach(Text t in text_gun_num)
