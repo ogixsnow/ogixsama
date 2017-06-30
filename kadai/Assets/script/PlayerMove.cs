9330b6e8e09352874681897c3d1af27a2b6f828d
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-30 09:46:49 +0900

    武器切り替えと攻撃方法の修正,playermoveスクリプトを修正

--- kadai/Assets/script/PlayerMove.cs
+++ kadai/Assets/script/PlayerMove.cs
@@ -3,33 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    private CharacterController charaCon;
-    private Vector3             move = Vector3.zero;
-    private float               speed = 2.0f;
-    private float               jumpPower = 2.5f;
-    private float               rotationSpeed = 180.0f;
-    private const float         GRAVITY = 9.8f;
-    private bool                moveType = true;
+    private CharacterController charaCon;               //キャラクターコンポーネントの変数
+    private Vector3             move = Vector3.zero;    //プレイヤー移動量
+    private float               speed = 2.0f;           //プレイヤー移動速度
+    private float               jumpPower = 2.5f;       //プレイヤーの跳躍力
+    private float               rotationSpeed = 180.0f; //プレイヤー回転速度
+    private const float         GRAVITY = 9.8f;         //重力
+    private bool                moveType = true;        //1人称視点の時:true 3人称視点の時:false
 
-    public GameObject targetEnemy = null;
-    public GameObject prefab_hitEffect1;
-    private Vector3 attackPoint;
-    private Weapon weapon;
+    public GameObject targetEnemy = null;               //Enemy格納用変数
+    public GameObject prefab_hitEffect1;                //プレイヤー攻撃時、ヒットエフェクト
+    private Vector3 attackPoint;                        //攻撃箇所（位置）
+    private Weapon weapon;                              //武器
 
     
     void Start () {
-        weapon = new Weapon();
+        weapon = new Weapon();                          //武器のメモリを確保し、初期化
         charaCon = GetComponent<CharacterController>();
 	}
 
 
     void Update() {
-        if (Input.GetMouseButtonDown(1))
-        {
-            weapon.changeWeapon();
-        }
-        setTargetEnemy();
-        attack_LClick();
+        setTargetEnemy();                                       //Enemy(ターゲット)の情報を取得
+        if (Input.GetMouseButtonDown(0)) { attack_weapon(); }   //左クリックで武器攻撃
+        if (Input.GetMouseButtonDown(1)) { change_weapon(); }   //右クリックで武器変更
 
         if (moveType)
         {
@@ -39,6 +36,28 @@ public class PlayerMove : MonoBehaviour {
         {
             playerMove_3Parson();
         }
+    }
+
+    //武器攻撃
+    private void attack_weapon()
+    {
+        switch (weapon.getType())
+        {
+            case 0:
+                if(targetEnemy != null)
+                {
+                    GameObject effect = Instantiate(prefab_hitEffect1, attackPoint, Quaternion.identity) as GameObject;
+                    Destroy(effect, 0.2f);
+                    Destroy(targetEnemy);
+                }
+                break;
+        }
+    }
+
+    //武器変更
+    private void change_weapon()
+    {
+        weapon.changeWeapon();
     }
 
     //視点切り替え
@@ -109,6 +128,7 @@ public class PlayerMove : MonoBehaviour {
         charaCon.Move(move * Time.deltaTime);
     }
 
+    //Enemy(ターゲット)情報を取得
     private void setTargetEnemy()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -126,16 +146,5 @@ public class PlayerMove : MonoBehaviour {
         targetEnemy = null;
     }
 
-    private void attack_LClick()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if(targetEnemy != null)
-            {
-                GameObject effect = Instantiate(prefab_hitEffect1, attackPoint, Quaternion.identity) as GameObject;
-                Destroy(effect, 0.2f);
-                Destroy(targetEnemy);
-            }
-        }
-    }
+   
 }
