f69e14fa558a8f1a8b92d5824a9f02b43c23d7b9
Big-West <oonishi_mirai@mhsolutions.co.jp>
2017-06-30 14:50:36 +0900

    Uiスクリプトを作成

---
+++ kadai/Assets/script/Ui.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui : MonoBehaviour {
+    public Text[] text_gun_num;
+
+	//残段数を変更
+    public void changeText_GunNum(int num)
+    {
+        foreach(Text t in text_gun_num)
+        {
+            if(t != null)
+            {
+                t.text = "残弾:" + num;
+            }
+        }
+    }
+}
